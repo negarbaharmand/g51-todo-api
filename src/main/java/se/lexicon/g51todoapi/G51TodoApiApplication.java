8d12253fbd748ab4870bbb3a05be8e36887feadd
Negar Baharmand <negarbaharmand@outlook.com>
2024-10-01 09:35:11 +0200

    first commit

---
+++ src/main/java/se/lexicon/g51todoapi/G51TodoApiApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g51todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G51TodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G51TodoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g51todoapi/G51TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g51todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G51TodoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
